0279dea2d608b094866f0fbbfdf704be03ffb450
NemasisII <computer_sci_45@hotmail.com>
2020-08-14 12:05:31 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'zKPE2kCxxktSNYqAllCL4tohPoDQp9+ZubkOMCGAyKZW4hgAwLPEvDEC0VXNC2aqpbyoIBbs33/LhTvSFppRkCj1cElkVBI28cs9cxnhSK7MxWZyorH7L2aLCb6N9jHbyWFU0jQU2zxWIcVIzHotZgdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
